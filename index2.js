192df2b09dfc47af95f597417db8b09cabd11780
Wilt Rovira <wrovira@gmail.com>
2017-09-04 08:58:21 -0500

    Se presentan 2 formas de guardar: create y save.

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 var mongoose =  require("mongoose");
 
-// // //conecta la promesa
+//conecta la promesa
 var Promise = require("bluebird");
 Promise.promisifyAll(require("mongoose"));
 mongoose.Promise = Promise;
@@ -10,12 +10,15 @@ mongoose.Promise = Promise;
 var promiseAbrirConexion = new Promise (function(resolve, reject){
 	var conn = mongoose.createConnection("localhost","senquiu",27017, function(err){
 		if (err){
+			conn.close();
 			reject (err);
 		}
 		resolve (conn);
 	});
 }).then(function(conn){
 	return promiseGuardarPromotor (conn);	
+}).then(function(){
+	return "Todo se completó con éxito";	
 }).catch(function(err){
 	console.log("Error en la conexión --> " + err.message);
 });
@@ -27,19 +30,21 @@ var promiseGuardarPromotor = function(conn){
 		var promotorSchema = new mongoose.Schema({
 			primerNombre : String,
 			primerApellido : String,
-			permalink: String
+			permalink : String,
+			correoElectronico : String
 		});
 		var Promotor = conn.model("Promotor", promotorSchema, "promotores");
 		var promotor = new Promotor({
 			//asigno los datos del promotor
-			primerNombre : "Condorito",
-			primerApellido : "López",
-			permalink: "condoritolopez"
+			primerNombre : "James",
+			primerApellido : "Rodríguez",
+			permalink : "jamesrodriguez",
+			correoElectronico : "james@example.com"
 		});
 		console.log(promotor.primerNombre + " " + promotor.primerApellido);
-		
-		//utiliza la función create del modelo
-		Promotor.create(promotor, function(err, promotor){
+
+		//utiliza ls función save
+		promotor.save(function(err){
 			if (err){
 				reject(err);
 			}else{
@@ -47,6 +52,7 @@ var promiseGuardarPromotor = function(conn){
 				conn.close();
 			}
 		});
+
 	});
 };
 promiseAbrirConexion;
